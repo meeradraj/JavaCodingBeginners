e40d172faa91aabd8f345e6076d14185fdedf049
meeradraj <47224227+meeradraj@users.noreply.github.com>
2019-03-19 17:30:22 -0700

    Created a new Hello World Prgm

---
+++ HelloJava.java
@@ -0,0 +1,17 @@
+public class HelloJava {
+
+
+    public static void main(String Args[]){
+
+        System.out.println("Hello World");
+        System.out.println("Welcome to JAVA Programming");
+        System.out.println("My first JAVA program");
+        System.out.printf("Welcome %s", "Meera\n");
+        System.out.printf("I slept for %d hours! Am I?",8);
+        int i = 10;
+        System.out.println("i = " + i);
+        int j = 20;
+        System.out.println("j = " + j);
+
+    }
+}
